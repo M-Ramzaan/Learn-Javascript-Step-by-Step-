894ffca78d9a4d10d55104c65a576123330aaca9
M-Ramzaan <ramzanakram1875@outlook.com>
2023-12-21 07:37:00 +0500

    Update Lec No 5

--- Lecture No 05/ch_5.js
+++ Lecture No 05/ch_5.js
@@ -49,6 +49,44 @@
 // }
 
 // //Modern JS way to write function
-// const arrowmul = (a, b) => {
+// let = arrowmul = (a, b) => {
 //   return a * b;
 // };
+
+//Practice Question
+//Count Vowels in a string (Normal Function Method)
+// function countVowels(str) {
+//   let count = 0;
+//   for (const char of str) {
+//     if (
+//       char === "a" ||
+//       char === "e" ||
+//       char === "i" ||
+//       char === "o" ||
+//       char === "u"
+//     ) {
+//       count++;
+//     }
+//     console.log(char);
+//   }
+
+//   console.log(count);
+// }
+
+//Count Vowels in a string (Arrow Function Method)
+const countVow = (str) => {
+  let count = 0;
+  for (const char of str) {
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+
+  return count;
+};
