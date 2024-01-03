23299d5aaf853043a293b2c31b477213c9e046dd
M-Ramzaan <ramzanakram1875@outlook.com>
2024-01-03 07:51:18 +0500

    Learning JS Ch-08

--- Lecture No 08/Ch_8.js
+++ Lecture No 08/Ch_8.js
@@ -1,17 +1,70 @@
 //Event Handling via JS;
 
-let btn = document.querySelector("btn2");
-btn2.onclick = () => {
-  console.log("A button was clicked");
-  let a = 1;
-  a++;
-  console.log(a);
-};
+// let btn = document.querySelector("btn2");
+// btn2.onclick = () => {
+//   console.log("A button was clicked");
+//   let a = 1;
+//   a++;
+//   console.log(a);
+// };
 
-let div = document.querySelector("div2");
-div2.onmouseover = () => {
-  console.log("You are inside div");
-};
+// let div = document.querySelector("div2");
+// div2.onmouseover = () => {
+//   console.log("You are inside div");
+// };
 //Important Point
 //JS event hadnling have more prioirty than the HTML inline event hadnling.
 //In JS if you re write an event for an same element the lastet written event work not the old one.
+
+//1-Event Objects
+//let btn2 = document.querySelector("#btn2");
+// btn2.onclick = (evt) => {
+//   console.log(evt); //It print the event object.
+//   console.log(evt.type); //It print the event type.
+//   console.log(evt.target); //It print the element where the event is set.
+//   console.log(evt.clientX, evt.clientY); //It print the place on web where the event is occuring.
+// };
+
+// let div = document.querySelector("div2");
+// div2.onmouseover = (evt) => {
+//   console.log("You are inside div");
+//   console.log(evt); //It print the event object.
+//   console.log(evt.type); //It print the event type.
+//   console.log(evt.target); //It print the element where the event is set.
+//   console.log(evt.clientX, evt.clientY); //It print the place on web where the event is occuring.
+// };
+
+//2-Event Listner
+//Use it in following way
+//node.addEventListner(event, callback)
+//node.removeEventListner(event, callback)
+let btn2 = document.querySelector("#btn2");
+// btn2.onclick = (evt) => {
+//   console.log(evt); //It print the event object.
+//   console.log(evt.type); //It print the event type.
+//   console.log(evt.target); //It print the element where the event is set.
+//   console.log(evt.clientX, evt.clientY); //It print the place on web where the event is occuring.
+// };
+btn2.addEventListener("click", (evt) => {
+  console.log("Button 01 was clicked - handler 01");
+  // console.log(evt);
+  // console.log(evt.type);
+  // console.log(evt.target);
+  // console.log(evt.clientX, evt.clientY);
+});
+
+btn2.addEventListener("click", () => {
+  console.log("Button 01 was clicked - handler 02");
+});
+
+const handler3 = () => {
+  console.log("Button 01 was clicked - handler 03");
+};
+
+btn2.addEventListener("click", handler3);
+
+btn2.removeEventListener("click", handler3);
+
+btn2.addEventListener("click", () => {
+  console.log("Button 01 was clicked - handler 04");
+});
